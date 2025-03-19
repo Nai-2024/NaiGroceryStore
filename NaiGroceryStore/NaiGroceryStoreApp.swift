09fa189cc6c76216cadcd454721abc483b8d01e7
Din <Din.Salehy@triosstudent.com>
2025-03-19 11:06:19 -0400

    Initial Commit

---
+++ NaiGroceryStore/NaiGroceryStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  NaiGroceryStoreApp.swift
+//  NaiGroceryStore
+//
+//  Created by Din Salehy on 2025-03-19.
+//
+
+import SwiftUI
+
+@main
+struct NaiGroceryStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
